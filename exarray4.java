e7cefcadb7a0030217360b0831a5e1688af94251
N4N0-GH05TL1N3 <gh0stsync.rt@gmail.com>
2016-03-01 09:21:15 +0700

    Create exarray4.java

---
+++ exarray4.java
@@ -0,0 +1,12 @@
+class exarray4 {
+	public static void main(String[] args) {
+	int []num = {3,8,5,4,6};
+	double r = Math.random() * 4;
+	int i = (int) r;
+	System.out.println("Random index is : " + i);
+	System.out.println("Value os index : " + num[i]);
+
+			
+	
+	}
+}
