45523389e2a54bdf06d76d4f838dcd2b4719ed84
N4N0-GH05TL1N3 <gh0stsync.rt@gmail.com>
2016-03-01 09:54:52 +0700

    Create exarray5.java

---
+++ exarray5.java
@@ -0,0 +1,14 @@
+class exarray5 {
+	public static void main(String[] args) {
+	String []member = {"Thor", "Hulk", "SpiderMan","SuperMan","Batman","Deadpool","Ghost Rider","Conan","Neo","Punisher"};
+	double r = Math.random() * 9;
+	int i = (int) r;
+	System.out.println("Lucky Draw is : " + member[i]);
+
+
+
+
+			
+	
+	}
+}
