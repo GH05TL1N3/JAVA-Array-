4dca3b380a5d650c31b0b43d1d42c852be24dab8
N4N0-GH05TL1N3 <gh0stsync.rt@gmail.com>
2016-03-01 15:11:52 +0700

    Update exarray7.java

--- exarray7.java
+++ exarray7.java
@@ -1,18 +1,19 @@
 class exarray7 {
 	public static void main(String[] args) {
 		int []num = new int[100];
+		int size = num.length;
 		int i,x,j,k,temp,m;
 		double r;
 		//------------------------------------------------------
-		for(i=0;i<=100-1;i++){
+		for(i=0;i<=size-1;i++){
 			r = Math.random() * 100;
 			x = (int) r;
 			num[i]=x;
 			System.out.print(num[i] + " ");
 				}
 		//------------------------------------------------------
-		for(j=0;j<=100-1;j++){
-			for(k=j+1;k<100;k++){
+		for(j=0;j<=size-1;j++){
+			for(k=j+1;k<size;k++){
 				if(num[j] > num[k]){
 					temp = num[j] ;
 					num[j] = num[k];
@@ -24,7 +25,7 @@ class exarray7 {
 
 		System.out.print("\n\n\n");
 		//------------------------------------------------------
-		for(m=0;m<=100-1;m++){
+		for(m=0;m<=size-1;m++){
 			System.out.print(num[m] + " ");
 		}
 	
