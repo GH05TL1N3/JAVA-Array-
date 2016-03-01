4ac7009d8a017b797b18e5b43b05aa56a5c67dfa
N4N0-GH05TL1N3 <gh0stsync.rt@gmail.com>
2016-03-01 08:54:40 +0700

    Create exarray3.java

---
+++ exarray3.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class exarray3 {
+	public static void main(String[] args) {
+	int []num = {3,8,5,4,6};
+	int i;
+	Scanner sc = new Scanner(System.in);
+	System.out.print("Enter index of array : ");
+	i = sc.nextInt();
+	System.out.println("Index : " + i + " is : " + num[i]);
+
+	
+	}
+}
